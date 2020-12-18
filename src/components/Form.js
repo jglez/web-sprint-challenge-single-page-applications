a5dbe9e8f535e3c8d502981670f625f85cd469b3
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-18 11:42:48 -0600

    Added Size dropdown, toppings checklist, delivery address and special instructions fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,9 +10,55 @@ export default function Form() {
 
       <form>
 
-        <label><b>Delivery Address</b>&nbsp;
+        <label>Delivery Address&nbsp;
           <input
             name='delivery'
+            type='text'
+          />
+        </label>
+
+        <label>Size&nbsp;
+          <select name='size'>
+            <option>--------</option>
+            <option>Small</option>
+            <option>Medium</option>
+            <option>Large</option>
+            <option>Family</option>
+          </select>
+        </label>
+
+        <label>Pepperoni
+          <input
+            name='pepperoni'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Pineapple
+          <input
+            name='pineapple'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Chicken
+          <input
+            name='chicken'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Bacon
+          <input
+            name='bacon'
+            type='checkbox'
+          />
+        </label>
+
+
+        <label>Special Instructions&nbsp;
+          <input
+            name='instructions'
             type='text'
           />
         </label>
